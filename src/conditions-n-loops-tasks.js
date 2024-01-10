685754b4eef7e550378df26c3fe9acde6a4e34eb
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-01-11 02:14:01 +0600

    feat: add isIsoscelesTriangle

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -91,8 +91,13 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a + b > c) {
+    if (a === b || b === c || a === c) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
